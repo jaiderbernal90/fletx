41ab336ef92368c154c675125da30bce912e2730
jaiderbernal90 <64909036+jaiderbernal90@users.noreply.github.com>
2025-04-19 22:09:38 -0500

    feat: add basic products module

---
+++ src/modules/products/entities/product.entity.ts
@@ -0,0 +1,18 @@
+import { Company } from '@/modules/companies/entities/company.entity';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @ManyToOne(() => Company, company => company.products)
+  @JoinColumn({ name: 'company_id' })
+  company: Company;
+}
